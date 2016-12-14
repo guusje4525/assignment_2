a3c97cef4205ead77e51cc26773995b162f6247e
Guus <guusvanwalstijn@gmail.com>
2016-12-14 21:52:57 +0100

    Reduced dependency's between the UI and train services(2)

--- src/Service/TrainService.java
+++ src/Service/TrainService.java
@@ -1,11 +1,6 @@
 package Service;
 
 import Controllers.Controller;
-import Trains.Train;
-import Trains.TrainStorage;
-import Trains.Wagon;
-
-import java.util.ArrayList;
 
 /**
  * Created by Guus on 12/12/2016.
@@ -14,19 +9,19 @@ public class TrainService {
 
     public static void main(String[] args){
 
-        TrainStorage.addTrain("trein1");
-        TrainStorage.addTrain("trein2");
-        TrainStorage.addTrain("trein3");
+        Controller.addTrain("trein1");
+        Controller.addTrain("trein2");
+        Controller.addTrain("trein3");
 
-        TrainStorage.addWagon("trein1", "wagon1", 50);
-        TrainStorage.addWagon("trein1", "wagon2", 30);
-        TrainStorage.addWagon("trein2", "wagon3", 10);
-        TrainStorage.addWagon("trein2", "wagon4");
-        TrainStorage.addWagon("trein3", "wagon5");
-        TrainStorage.addWagon("trein3", "wagon6");
-        TrainStorage.addWagon("trein3", "wagon7");
-        TrainStorage.addWagon("trein5", "wagon70");
-        TrainStorage.addWagon("trein78", "wagon70");
+        Controller.addWagon("trein1", "wagon1", 50);
+        Controller.addWagon("trein1", "wagon2", 30);
+        Controller.addWagon("trein2", "wagon3", 10);
+        Controller.addWagon("trein2", "wagon4");
+        Controller.addWagon("trein3", "wagon5");
+        Controller.addWagon("trein3", "wagon6");
+        Controller.addWagon("trein3", "wagon7");
+        Controller.addWagon("trein5", "wagon70");
+        Controller.addWagon("trein78", "wagon70");
 
         Controller.start();
 
